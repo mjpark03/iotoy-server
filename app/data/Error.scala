eb7e456e419e53cd24d9fce7575753ef270e97ff
mjpark03 <mjpark03@gmail.com>
2017-09-19 17:14:37 +0900

    Remove unused error messages

--- app/data/Error.scala
+++ app/data/Error.scala
@@ -10,14 +10,11 @@ object ErrorMessage {
   val NO_CUSTOMER = "There is no customer"
   val NO_LINKER = "There is no linker"
   val NO_HOST = "There is no host"
-  val NO_IOTOY_ID = "There is no iotoy id"
-  val NO_THING_TYPE = "There is no thing type"
-  val NO_THING_ID = "There is no thing id"
-  val NO_COMMAND = "There is no command"
 
   val INVALID_AUTH_NUMBER = "auth number is not correct"
 
   val FAIL_SMS_SEND = "sending SMS is failed"
   val FAIL_DB_SQL_EXECUTION = "the execution of db sql is failed"
 
+  val UNKNOWN_ERROR = "There is unknown error"
 }
