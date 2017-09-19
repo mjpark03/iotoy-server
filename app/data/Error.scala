dfc6a7322db0320d929e6fb612217549fe458418
mjpark03 <mjpark03@gmail.com>
2017-09-19 16:48:34 +0900

    Add error message for validating request params

--- app/data/Error.scala
+++ app/data/Error.scala
@@ -10,6 +10,10 @@ object ErrorMessage {
   val NO_CUSTOMER = "There is no customer"
   val NO_LINKER = "There is no linker"
   val NO_HOST = "There is no host"
+  val NO_IOTOY_ID = "There is no iotoy id"
+  val NO_THING_TYPE = "There is no thing type"
+  val NO_THING_ID = "There is no thing id"
+  val NO_COMMAND = "There is no command"
 
   val INVALID_AUTH_NUMBER = "auth number is not correct"
 
